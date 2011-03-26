3776334f73038d4044416ce9d4f8cb3e1a13ec11
Pieter Colpaert <pieterc@member.fsf.org>
2011-03-26 15:09:35 +0100

    awesomeness ahead

--- datalayer.php
+++ datalayer.php
@@ -4,24 +4,38 @@
    */
 
 function getClosestStation($x,$y){
-$r = new HttpRequest('http://api.irail.be/stations/?lang=NL&format=json', HttpRequest::METH_GET);
-$r->setOptions(array('lastmodified' => filemtime('local.rss')));
-$r->addQueryData(array('category' => 3));
-try {
-    $r->send();
-    if ($r->getResponseCode() == 200) {
-        file_put_contents('local.rss', $r->getResponseBody());
-    }
-} catch (HttpException $ex) {
-    echo $ex;
-}
-
-}
+     $r = new HttpRequest('http://api.irail.be/stations/?lang=NL&format=json', HttpRequest::METH_GET);
+     try {
+	  $r->send();
+	  if ($r->getResponseCode() == 200) {
+	       $json = json_decode($r->getResponseBody());
+	       return calculateClosest($json["station"],$x,$y);
+	  }
+     } catch (HttpException $ex) {
+	  echo $ex;
+     }
+  }
 
 function getConnection($stationvan,$stationnaar){
-     
+     $r = new HttpRequest('http://api.irail.be/connections/?lang=NL&format=json&from=' . url_encode($stationvan) . 'to=' . url_encode($stationnaar), HttpRequest::METH_GET);
+     try {
+	  $r->send();
+	  if ($r->getResponseCode() == 200) {
+	       $json = json_decode($r->getResponseBody());
+	       return calculateClosest($json["station"],$x,$y);
+	  }
+     } catch (HttpException $ex) {
+	  echo $ex;
+     }
 
 }
+
+
+function calculateClosest($stations,$x,$y){
+     //TODO
+
+}
+
 
 
 ?>
